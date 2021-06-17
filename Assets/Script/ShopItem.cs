c7eda0068f19c04a5d99daabbb1d2bcfca99f79b
Jang Gyu Hyun <jgh950829@gmail.com>
2021-06-18 06:07:41 +0900

    완성

--- Assets/Script/ShopItem.cs
+++ Assets/Script/ShopItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShopItem : MonoBehaviour
 {
@@ -14,10 +15,24 @@ public class ShopItem : MonoBehaviour
 
     public int healthUpgradeAmount;
 
+    public Gun[] potentialGuns;
+    private Gun theGun;
+    public SpriteRenderer gunSprite;
+    public Text infoText;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(isWeapon)
+        {
+            int selectGun = Random.Range(0, potentialGuns.Length);
+            theGun = potentialGuns[selectGun];
+
+            gunSprite.sprite = theGun.gunShopSprite;
+            infoText.text = theGun.weaponName + "\n - " + theGun.itemCost + " Gold - ";
+            itemCost = theGun.itemCost;
+
+        }
     }
 
     // Update is called once per frame
@@ -39,6 +54,19 @@ public class ShopItem : MonoBehaviour
                     if(isHealthUpgrade)
                     {
                         PlayerHealthController.instance.IncreaseMaxHealth(healthUpgradeAmount);
+                    }
+
+                    if(isWeapon)
+                    {
+                        Gun gunClone = Instantiate(theGun);
+                        gunClone.transform.parent = PlayerController.instance.gunArm;
+                        gunClone.transform.position = PlayerController.instance.gunArm.position;
+                        gunClone.transform.localRotation = Quaternion.Euler(Vector3.zero);
+                        gunClone.transform.localScale = Vector3.one;
+
+                        PlayerController.instance.availableGuns.Add(gunClone);
+                        PlayerController.instance.currentGun = PlayerController.instance.availableGuns.Count - 1;
+                        PlayerController.instance.SwitchGun();
                     }
 
                     gameObject.SetActive(false);
